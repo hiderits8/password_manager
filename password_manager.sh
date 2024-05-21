0a59fa47eca318a57adfc72be3ea596ebd892316
hiderits8 <wataru.ts8.hideri@gmail.com>
2024-05-21 22:34:40 +0900

    完成

--- password_manager.sh
+++ password_manager.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "パスワードマネージャーへようこそ！"
+echo パスワードマネージャーへようこそ！
 
 # 作業を選択、$taskに代入
 read -p '次の選択肢から入力してください(Add Password/Get Password/Exit):' task
@@ -24,14 +24,28 @@ while [ "$task" != 'Exit' ]; do
         # 入力された情報をpassword_manager.txtに保存
         echo $service:$user:$password >>password_manager.txt
 
-        echo "パスワードの追加は成功しました。"
+        echo パスワードの追加は成功しました。
         read -p '次の選択肢から入力してください(Add Password/Get Password/Exit):' task
     # 出力
     elif [ "$task" = "Get Password" ]; then
-        read -p 'サービス名を入力してください' service
-        cat password_manager.txt | grep $service
+        read -p 'サービス名を入力してください' search
+
+        if cat password_manager.txt | cut -d : -f 1 | grep -x $search >/dev/null; then
+            service=$(cat password_manager.txt | grep ^$search | cut -d : -f 1)
+            echo "サービス名:$service"
+            user=$(cat password_manager.txt | grep ^$search | cut -d : -f 2)
+            echo "ユーザー名:$user"
+            password=$(cat password_manager.txt | grep ^$search | cut -d : -f 3)
+            echo "パスワード:$password"
+        else
+            echo そのサービスは登録されていません。
+        fi
+        read -p '次の選択肢から入力してください(Add Password/Get Password/Exit):' task
+
+    else
+        echo 入力が間違えています。Add Password/Get Password/Exit から入力してください。
         read -p '次の選択肢から入力してください(Add Password/Get Password/Exit):' task
     fi
 
 done
-echo 'Thank you!'
+echo Thank you!
